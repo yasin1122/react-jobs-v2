84f0a52e50925d9e3a6d673ccaed9ba4a46eb928
Yasin Cakal <whoisyasin@gmail.com>
2025-04-11 11:42:30 -0700

    configured tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 const App = () => {
-  return <div className='text-3xl font-bold underline'>App</div>
+  return <div className='text-5xl font-bold underline text-red-500'>App</div>
 }
 
 export default App
