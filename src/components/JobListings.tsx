975664e0df293001ff2505b67356b48743149f1c
Yasin Cakal <whoisyasin@gmail.com>
2025-04-12 09:07:04 -0700

    job listings

--- src/components/JobListings.tsx
+++ src/components/JobListings.tsx
@@ -3,7 +3,22 @@ import jobs from '../jobs.json'
 console.log(jobs)
 
 const JobListings = () => {
-  return <div>JobListings</div>
+  return (
+    <section className='bg-blue-50 px-4 py-10'>
+      <div className='container-xl lg:container m-auto'>
+        <h2 className='text-3xl font-bold text-indigo-500 mb-6 text-center'>
+          Browse Jobs
+        </h2>
+        <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+          {jobs.map(job => (
+            <div className='bg-white'>
+              <div className='p-4'></div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default JobListings
