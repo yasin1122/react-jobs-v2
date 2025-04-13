b112d6102bc5acd42370f7ac724fd9f5703f43d2
Yasin Cakal <whoisyasin@gmail.com>
2025-04-12 20:46:35 -0700

    added react icons

--- src/components/JobListing.tsx
+++ src/components/JobListing.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { FaMapMarker } from 'react-icons/fa'
 
 interface Job {
   id: string
@@ -35,7 +36,7 @@ const JobListing = ({ job }: { job: Job }) => {
         <div className='border border-gray-100 mb-5'></div>
         <div className='flex flex-col lg:flex-row justify-between mb-4'>
           <div className='text-orange-700 mb-3'>
-            <i className='fa-solid fa-location-dot text-lg'></i>
+            <FaMapMarker className='inline text-lg mb-1 mr-1' />
             {job.location}
           </div>
           <a
