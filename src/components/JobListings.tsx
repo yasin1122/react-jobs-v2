def18c0686fea68619f675af788f1f6176a27a24
Yasin Cakal <whoisyasin@gmail.com>
2025-04-13 11:15:36 -0700

    implemented api

--- src/components/JobListings.tsx
+++ src/components/JobListings.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import JobListing from './JobListing'
+import Spinner from './Spinner'
 
 const JobListings = ({ isHome = false }) => {
   type Job = {
@@ -16,8 +17,10 @@ const JobListings = ({ isHome = false }) => {
 
   useEffect(() => {
     const fetchJobs = async () => {
+      const apiUrl = isHome ? 'api/jobs?_limit=3' : 'api/jobs'
+
       try {
-        const res = await fetch('http://localhost:8000/jobs')
+        const res = await fetch(apiUrl)
         const data = await res.json()
         setJobs(data)
       } catch (error) {
@@ -35,20 +38,19 @@ const JobListings = ({ isHome = false }) => {
         <h2 className='text-3xl font-bold text-indigo-500 mb-6 text-center'>
           {isHome ? 'Recent Jobs' : 'Browse Jobs'}
         </h2>
-        <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
-          {loading ? (
-            <h2>Loading...</h2>
-          ) : (
-            <>
-              {jobs.map(job => (
-                <JobListing
-                  key={job.id}
-                  job={job}
-                />
-              ))}
-            </>
-          )}
-        </div>
+
+        {loading ? (
+          <Spinner loading={loading} />
+        ) : (
+          <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+            {jobs.map(job => (
+              <JobListing
+                key={job.id}
+                job={job}
+              />
+            ))}
+          </div>
+        )}
       </div>
     </section>
   )
--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,13 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    port: 3000
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, '')
+      }
+    }
   }
 })
